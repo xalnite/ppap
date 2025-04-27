11fd6f9bb7ca784f5717250bd471a8f462c19b25
xalnite <xalnite@gmail.com>
2025-04-27 17:12:12 +0900

    feat: PPAP Helper initial implementation

---
+++ ppap.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+import random
+import secrets
+import sys
+
+import pyminizip
+
+FILENAME_MAX_SUFFIX = 100
+COMPRESS_LEVEL = 9
+PASSWORD_MAX_LENGTH = 99
+PASSWORD_ILLEGAL_CHAR = set([ord(c) for c in "'\""])
+PASSWORD_CHARSET = "".join(chr(c) for c in range(0x21, 0x7f) if c not in PASSWORD_ILLEGAL_CHAR)
+MESSAGE_TEMPLATE = "generated password is {}"
+
+def create_text_file(text: str, content: str) -> None:
+    # Create a text file with the specified content
+    with open(text, "w", encoding="utf-8") as file:
+        file.write(content)
+
+def encrypt_file(zip: str, password: str, contents: list[str]) -> None:
+    # Encrypt a file with a password
+    pyminizip.compress(
+        contents[0].encode("cp932"), "", zip.encode("cp932"), password, COMPRESS_LEVEL
+    )
+
+def generate_password(length: int) -> str:
+    # Generate a password of a given length
+    if length > PASSWORD_MAX_LENGTH or length < 1:
+        length = PASSWORD_MAX_LENGTH
+    return "".join(
+        secrets.choice(PASSWORD_CHARSET) for i in range(length)
+    )
+
+def main(contents: list[str]) -> None:
+    # Main function to handle file encryption and password generation
+    top: str = contents[0]
+    (root, _) = os.path.splitext(top)
+    zip: str = root + ".zip"
+    txt: str = root + "-password.txt"
+    for n in range(FILENAME_MAX_SUFFIX):
+        if not os.path.exists(zip) and not os.path.exists(txt):
+            break
+        zip = f"{root}-{n}.zip"
+        txt = f"{root}-{n}-password.txt"
+    password: str = generate_password(random.randint(PASSWORD_MAX_LENGTH // 2, PASSWORD_MAX_LENGTH))
+    encrypt_file(zip, password, contents)
+    create_text_file(txt, MESSAGE_TEMPLATE.format(password))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
